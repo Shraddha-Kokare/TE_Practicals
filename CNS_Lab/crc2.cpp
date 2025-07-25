ac89e748d2fe96827ec01f3bd028c08e6a456711
Shraddha-Kokare <139088829+Shraddha-Kokare@users.noreply.github.com>
2025-07-25 13:02:35 +0530

    Update crc2.cpp

--- CNS_Lab/crc2.cpp
+++ CNS_Lab/crc2.cpp
@@ -28,33 +28,90 @@ string xorStrings(string str1, string str2)
 
 string getCodeWord(string binary_inp, string generator)
 {
-    string codeWord = "", xored = "";
-    int gen_len = generator.length();
-    string appended = binary_inp + string(gen_len - 1, '0');
-    string remainder = appended.substr(0, gen_len);
+    // string codeWord = "", xored = "";
+    // int gen_len = generator.length();
+    // string appended = binary_inp + string(gen_len - 1, '0');
+    // string remainder = appended.substr(0, gen_len);
 
-    for (int i = gen_len; i <= appended.length(); i++)
+    // cout << "\nAppended string: " << appended;
+    // // cout << "\nRemainder : " << remainder;
+
+    // for (int i = gen_len; i <= appended.length(); i++)
+    // {
+    //     cout << "\ni = " << i << "\tRemainder : " << remainder << "\tGenerator: " << generator;
+    //     if (remainder[i] == '1')
+    //     {
+    //         xored = xorStrings(remainder, generator);
+    //         if (i < appended.length() - 1)
+    //             remainder += appended[i];
+    //         else
+    //             remainder += '\0';
+    //     }
+    //     else
+    //     {
+    //         remainder = remainder.substr(1);
+    //         if (i < appended.length() - 1)
+    //             remainder += appended[i];
+    //         else
+    //             remainder += '\0';
+    //     }
+    // }
+    // cout << "\nRemainder : " << remainder;
+
+    // codeWord = binary_inp + remainder.substr(1);
+    // return codeWord;
+    int genLen = generator.length();
+    string appendedData = binary_inp + string(genLen - 1, '0');
+    string remainder = appendedData.substr(0, genLen);
+
+    for (int i = genLen; i <= appendedData.length(); i++)
     {
-        if (remainder[i] == '1')
+        if (remainder[0] == '1')
         {
-            xored = xorStrings(remainder, generator);
-            if (i < appended.length() - 1)
-                remainder += appended[i];
-            else
-                remainder += '\0';
+            remainder = xorStrings(generator, remainder) + (i < appendedData.length() ? appendedData[i] : '\0');
         }
         else
         {
-            remainder = remainder.substr(1);
-            if (i < appended.length() - 1)
-                remainder += appended[i];
-            else
-                remainder += '\0';
+            remainder = xorStrings(string(genLen, '0'), remainder) + (i < appendedData.length() ? appendedData[i] : '\0');
         }
     }
 
-    codeWord = binary_inp + remainder.substr(1);
-    return codeWord;
+    string crc = remainder.substr(0, genLen - 1);
+    return binary_inp + crc;
+}
+
+bool crcCheck(string received, string generator)
+{
+    int genLen = generator.length();
+    string remainder = received.substr(0, genLen);
+
+    for (int i = genLen; i <= received.length(); i++)
+    {
+        if (remainder[0] == '1')
+        {
+            remainder = xorStrings(generator, remainder) + (i < received.length() ? received[i] : '\0');
+        }
+        else
+        {
+            remainder = xorStrings(string(genLen, '0'), remainder) + (i < received.length() ? received[i] : '\0');
+        }
+    }
+
+    for (char c : remainder.substr(0, genLen - 1))
+    {
+        if (c != '0')
+            return false;
+    }
+    return true;
+}
+
+string injectNoise(string codeword)
+{
+    srand(time(0));
+    int pos = rand() % codeword.length();
+    codeword[pos] = (codeword[pos] == '0') ? '1' : '0';
+    cout << "Noise injected at bit position: " << pos << endl;
+    return codeword;
 }
 
 int main()
@@ -67,6 +124,35 @@ int main()
 
     cout << "\nEnter Generator bits: ";
     cin >> generator;
+
+    string codeWord = getCodeWord(binary_rep, generator);
+    cout << "\nCodeword is :" << codeWord;
+
+    char choice;
+    cout << "\nDo you want to inject noise? (y/n): ";
+    cin >> choice;
+
+    string received = codeWord;
+
+    if (choice == 'y' || choice == 'Y')
+    {
+        received = injectNoise(received);
+        cout << "Received codeword with noise: " << received << endl;
+    }
+    else
+    {
+        cout << "Received codeword (no noise): " << received << endl;
+    }
+
+    cout << "\nReceiver Side :\n" << endl;
+    if (crcCheck(received, generator))
+    {
+        cout << " No Error Detected (Data is correct)." << endl;
+    }
+    else
+    {
+        cout << " Error Detected in Data!" << endl;
+    }
 
     return 0;
 }
