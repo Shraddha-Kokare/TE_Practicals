73628304df37297c3af0930b00bb8c75c5f8092f
Shraddha-Kokare <139088829+Shraddha-Kokare@users.noreply.github.com>
2025-08-29 12:49:42 +0530

    Update crc.cpp

--- CNS_Lab/crc.cpp
+++ CNS_Lab/crc.cpp
@@ -1,68 +1,112 @@
 #include <iostream>
-#include <bitset>
 #include <string>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
-
-// Convert input string to binary using ASCII values
-string stringToBinary(const string& input) {
-    string binary = "";
-    for (char ch : input) {
-        binary += bitset<8>(ch).to_string();  // 8-bit binary
+string toBinary(string input)
+{
+    string binary_rep = "";
+    for (char ch : input)
+    {
+        for (int i = 7; i >= 0; i--)
+        {
+            binary_rep += ((ch >> i) & 1) ? '1' : '0';
+        }
     }
-    return binary;
+
+    return binary_rep;
 }
 
-// XOR operation between two binary strings
-string xorStrings(string a, string b) {
+string xorStrings(string str1, string str2)
+{
     string result = "";
-    for (int i = 1; i < b.length(); i++) {
-        result += (a[i] == b[i]) ? '0' : '1';
+    for (int i = 1; i < str2.length(); i++)
+    {
+        result += ((str1[i] == str2[i]) ? '0' : '1');
     }
+
     return result;
 }
 
-// CRC Encoding Function
-string crcEncode(string binaryData, string generator) {
+string getCodeWord(string binary_inp, string generator)
+{
+    // string codeWord = "", xored = "";
+    // int gen_len = generator.length();
+    // string appended = binary_inp + string(gen_len - 1, '0');
+    // string remainder = appended.substr(0, gen_len);
+
+    // cout << "\nAppended string: " << appended;
+    // // cout << "\nRemainder : " << remainder;
+
+    // for (int i = gen_len; i <= appended.length(); i++)
+    // {
+    //     cout << "\ni = " << i << "\tRemainder : " << remainder << "\tGenerator: " << generator;
+    //     if (remainder[i] == '1')
+    //     {
+    //         xored = xorStrings(remainder, generator);
+    //         if (i < appended.length() - 1)
+    //             remainder += appended[i];
+    //         else
+    //             remainder += '\0';
+    //     }
+    //     else
+    //     {
+    //         remainder = remainder.substr(1);
+    //         if (i < appended.length() - 1)
+    //             remainder += appended[i];
+    //         else
+    //             remainder += '\0';
+    //     }
+    // }
+    // cout << "\nRemainder : " << remainder;
+
+    // codeWord = binary_inp + remainder.substr(1);
+    // return codeWord;
     int genLen = generator.length();
-    string appendedData = binaryData + string(genLen - 1, '0');
+    string appendedData = binary_inp + string(genLen - 1, '0');
     string remainder = appendedData.substr(0, genLen);
 
-    for (int i = genLen; i <= appendedData.length(); i++) {
-        if (remainder[0] == '1') {
+    for (int i = genLen; i <= appendedData.length(); i++)
+    {
+        if (remainder[0] == '1')
+        {
             remainder = xorStrings(generator, remainder) + (i < appendedData.length() ? appendedData[i] : '\0');
-        } else {
+        }
+        else
+        {
             remainder = xorStrings(string(genLen, '0'), remainder) + (i < appendedData.length() ? appendedData[i] : '\0');
         }
     }
 
     string crc = remainder.substr(0, genLen - 1);
-    return binaryData + crc;
+    return binary_inp + crc;
 }
 
-// CRC Checking Function
-bool crcCheck(string received, string generator) {
+bool crcCheck(string received, string generator)
+{
     int genLen = generator.length();
     string remainder = received.substr(0, genLen);
 
-    for (int i = genLen; i <= received.length(); i++) {
-        if (remainder[0] == '1') {
+    for (int i = genLen; i <= received.length(); i++)
+    {
+        if (remainder[0] == '1')
+        {
             remainder = xorStrings(generator, remainder) + (i < received.length() ? received[i] : '\0');
-        } else {
+        }
+        else
+        {
             remainder = xorStrings(string(genLen, '0'), remainder) + (i < received.length() ? received[i] : '\0');
         }
     }
 
-    for (char c : remainder.substr(0, genLen - 1)) {
-        if (c != '0') return false;
+    for (char c : remainder.substr(0, genLen - 1))
+    {
+        if (c != '0')
+            return false;
     }
     return true;
 }
 
-// Function to inject noise (flip a random bit)
-string injectNoise(string codeword) {
+string injectNoise(string codeword)
+{
     srand(time(0));
     int pos = rand() % codeword.length();
     codeword[pos] = (codeword[pos] == '0') ? '1' : '0';
@@ -70,39 +114,43 @@ string injectNoise(string codeword) {
     return codeword;
 }
 
-// Main Function
-int main() {
-    string input, generator;
-    cout << "Enter input string: ";
-    getline(cin, input);
+int main()
+{
+    string inputString, generator;
+    cout << "\nEnter the input string: ";
+    cin >> inputString;
+    string binary_rep = toBinary(inputString);
+    cout << "\nBinary Representation :" << binary_rep;
 
-    cout << "Enter generator polynomial (binary): ";
+    cout << "\nEnter Generator bits: ";
     cin >> generator;
 
-    string binaryData = stringToBinary(input);
-    cout << "Binary representation of input: " << binaryData << endl;
+    string codeWord = getCodeWord(binary_rep, generator);
+    cout << "\nCodeword is :" << codeWord;
 
-    string encoded = crcEncode(binaryData, generator);
-    cout << "Transmitted Codeword (Data + CRC): " << encoded << endl;
-
-    // Ask user if they want to inject noise
     char choice;
     cout << "\nDo you want to inject noise? (y/n): ";
     cin >> choice;
 
-    string received = encoded;
-    if (choice == 'y' || choice == 'Y') {
+    string received = codeWord;
+
+    if (choice == 'y' || choice == 'Y')
+    {
         received = injectNoise(received);
         cout << "Received codeword with noise: " << received << endl;
-    } else {
+    }
+    else
+    {
         cout << "Received codeword (no noise): " << received << endl;
     }
 
-    // Check at receiver
-    cout << "\n--- Receiver Side ---" << endl;
-    if (crcCheck(received, generator)) {
+    cout << "\nReceiver Side :\n" << endl;
+    if (crcCheck(received, generator))
+    {
         cout << " No Error Detected (Data is correct)." << endl;
-    } else {
+    }
+    else
+    {
         cout << " Error Detected in Data!" << endl;
     }
 
